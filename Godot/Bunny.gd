1344f0edba98fa95a669422069d96c34af6e0b06
Lauren Chen <seal789ie@gmail.com>
2019-11-10 19:42:47 +0800

    adjust bunny action

--- Godot/Bunny.gd
+++ Godot/Bunny.gd
@@ -1,31 +1,35 @@
 extends Node2D
 
-var v = Vector2(randi() % 200 + 50, randi() % 200 + 50)
-const ay = 980
 var pos
+var speed = Vector2(randi() % 200 + 50, randi() % 200 + 50)
+var grav = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = Vector2(10, 10)
-	pos = position
 	set_process(true)
 
 func _process(delta):
-	v.y += ay * delta
-	pos += v * delta
+	var pos = position
+	var v =  speed
+	pos.x += v.x * delta
+	pos.y += v.y * delta
+	v.y += grav * delta
 	
-	if pos.y > 568:
-		v.y *= -0.85 
-		pos.y = 568
+	if pos.x > 1024:
+		v.x *= -1
+		pos.x = 1024
+	if pos.x < 0:
+		v.x *= -1
+		pos.x = 0
+	if pos.y > 600:
+		pos.y = 600
 		if randf() > 0.5:
-			v.y = -rand_range(32,1150)
-	elif pos.y < 0:
+			v.y = -(randi() % 1100 + 50)
+		else:
+			v.y *= -0.85
+	if pos.y < 0:
 		v.y = 0
 		pos.y = 0
-	elif pos.x > 1000:
-		v.x = -v.x
-		pos.x = 1000
-	elif pos.x < 0:
-		v.x = abs(v.x)
-		pos.x = 0
 	position = pos
+	speed = v
